fb6df44613e683a52aef3ce2541ee2194b3137f2
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2025-07-03 18:19:39 -0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    base: 'https://www.tecnoweb.org.bo/inf513/grupo23sc/proyecto2/public/build',
+    resolve: {
+        alias: {
+          '@': resolve(__dirname, 'resources/js'),
+        },
+      },
+});
